f59100826ff0becd33de806e6340b59bd71d9e19
LAPTOP-Q74S5O9J\user <mskim3324@gmail.com>
2022-11-30 14:54:57 +0900

    start

--- TESTmqtt/Core/Inc/mongoose_custom.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MG_ARCH MG_ARCH_NEWLIB
-#define MG_ENABLE_MIP 1
-#define MG_ENABLE_CUSTOM_MILLIS 1
-#define MG_ENABLE_FILE 0
-#define MG_ENABLE_PACKED_FS 1
---
+++ TESTmqtt/Debug/Core/Src/net.d
@@ -0,0 +1,4 @@
+Core/Src/net.o: ../Core/Src/net.c ../Core/Inc/mongoose.h \
+ ../Core/Inc/mongoose_custom.h
+../Core/Inc/mongoose.h:
+../Core/Inc/mongoose_custom.h:
